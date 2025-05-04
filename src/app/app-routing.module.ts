05f7773b33530e35dfc53d4f86c54ce6922641e1
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-05-04 13:24:48 +0530

    Removed unncessary pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,8 @@ import { LandingPageComponent } from './views/pages/landing-page/landing-page.co
 import { CoursePageComponent } from './views/pages/course-page/course-page.component';
 import { CoursePageDetailsComponent } from './views/pages/course-page-details/course-page-details.component';
 import { TeachingPageComponent } from './views/pages/teaching-page/teaching-page.component';
-import { EducatorRegistrationPageComponent } from './views/pages/educator/educator-registration-page/educator-registration-page.component';
 import { CourseCategoryComponent } from './views/pages/course-category/course-category.component';
 import { LearningPageComponent } from './views/pages/learning-page/learning-page.component';
-import { LearnerRegistrationPageComponent } from './views/pages/leaner/learner-registration-page/learner-registration-page.component';
-import { EducatorLoginPageComponent } from './views/pages/educator/educator-login-page/educator-login-page.component';
-import { LearnerLoginPageComponent } from './views/pages/leaner/learner-login-page/learner-login-page.component';
 const routes: Routes = [
   // Public routes
   { path: 'landing-page', component:  LandingPageComponent, title: 'Main page' },
@@ -20,10 +16,6 @@ const routes: Routes = [
   { path: 'courses/category', component: CourseCategoryComponent, title: 'Course page' },
   { path: 'learning', component:  LearningPageComponent, title: 'Course page' },
   { path: 'teaching', component:  TeachingPageComponent, title: 'Course page' },
-  { path: 'educator/registration-page', component:  EducatorRegistrationPageComponent, title: 'Course page' },
-  { path: 'educator/login-page', component:  EducatorLoginPageComponent, title: 'Course page' },
-  { path: 'learner/registration-page', component:  LearnerRegistrationPageComponent, title: 'Course page' },
-  { path: 'learner/login-page', component:  LearnerLoginPageComponent, title: 'Course page' },
   { path: '**', redirectTo: 'landing-page' }, // Fallback rou
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,8 @@ import { LayoutModule } from './views/layouts/layout.module';
 import { CoursePageComponent } from './views/pages/course-page/course-page.component';
 import { CoursePageDetailsComponent } from './views/pages/course-page-details/course-page-details.component';
 import { TeachingPageComponent } from './views/pages/teaching-page/teaching-page.component';
-import { LearnerMainPageComponent } from './views/pages/leaner/learner-main-page/learner-main-page.component';
-import { EducatorMainPageComponent } from './views/pages/educator/educator-main-page/educator-main-page.component';
 import { LearningPageComponent } from './views/pages/learning-page/learning-page.component';
-import { LearnerProfileSettingsPageComponent } from './views/pages/leaner/learner-profile-settings-page/learner-profile-settings-page.component';
-import { LearnerProfilePageComponent } from './views/pages/leaner/learner-profile-page/learner-profile-page.component';
-import { LearnerAccountSettingsPageComponent } from './views/pages/leaner/learner-account-settings-page/learner-account-settings-page.component';
-import { LearnerRegistrationPageComponent } from './views/pages/leaner/learner-registration-page/learner-registration-page.component';
-import { EducatorRegistrationPageComponent } from './views/pages/educator/educator-registration-page/educator-registration-page.component';
 import { CourseCategoryComponent } from './views/pages/course-category/course-category.component';
-import { LearnerLoginPageComponent } from './views/pages/leaner/learner-login-page/learner-login-page.component';
-import { EducatorLoginPageComponent } from './views/pages/educator/educator-login-page/educator-login-page.component';
 import { EducatorCreateCoursePageComponent } from './educator-create-course-page/educator-create-course-page.component';
 
 @NgModule({
@@ -28,17 +19,8 @@ import { EducatorCreateCoursePageComponent } from './educator-create-course-page
     CoursePageComponent,
     CoursePageDetailsComponent,
     TeachingPageComponent,
-    LearnerMainPageComponent,
-    EducatorMainPageComponent,
     LearningPageComponent,
-    LearnerProfileSettingsPageComponent,
-    LearnerProfilePageComponent,
-    LearnerAccountSettingsPageComponent,
-    LearnerRegistrationPageComponent,
-    EducatorRegistrationPageComponent,
     CourseCategoryComponent,
-    LearnerLoginPageComponent,
-    EducatorLoginPageComponent,
     EducatorCreateCoursePageComponent,
 
   ],
